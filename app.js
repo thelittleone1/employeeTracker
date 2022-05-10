550b8ec403d36424e68b0049928428cf3fe510a9
Urooj Qureshi <36391381+thelittleone1@users.noreply.github.com>
2022-05-09 21:58:45 -0700

    updated app.js, startmenu, and add employee

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
 // Constants
 const inquirer = require("inquirer");
-const mysql = require("mysql2");
+//const mysql = require("mysql2");
 
 // Required Files
 const startMenuEl = require("./lib/startMenu");
 
 // Need to create Connection to Mysql for Node
-const connection = mysql.createConnection({
-    host: "localhost",
-    user: "user",
-    password: "password",
-    database: "database name"
-});
-connection.connect((err) => {
-    if (err) throw err;
-    console.log("Connected");
-});
+// const connection = mysql.createConnection({
+//     host: "localhost",
+//     user: "user",
+//     password: "password",
+//     database: "database name"
+// });
+// connection.connect((err) => {
+//     if (err) throw err;
+//     console.log("Connected");
+// });
 
 startMenuEl();
