3e03336ed596529ee50cde2d257e35091068cf6a
Urooj Qureshi <36391381+thelittleone1@users.noreply.github.com>
2022-05-11 18:29:59 -0700

    nick helped me fix the schema and seeds!

--- db/schema.sql
+++ db/schema.sql
@@ -14,21 +14,18 @@ CREATE TABLE role (
     id INT PRIMARY KEY AUTO_INCREMENT,
     title VARCHAR(30) NOT NULL DEFAULT " ",
     salary DECIMAL NOT NULL,
-    department_id INT NOT NULL,
+    department_id INT,
     FOREIGN KEY(department_id)
     REFERENCES department(id)
     ON DELETE SET NULL
 );
 
 CREATE TABLE employee (
-    id INTO PRIMARY KEY AUTO_INCREMENT,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT,
-    manager_id INT,
-    FOREIGN KEY(role_id)
-    REFERENCES role(id)
+    FOREIGN KEY(role_id) REFERENCES role(id),
+    manager_id INT REFERENCES employee(id) 
     ON DELETE SET NULL
-    FOREIGN KEY (manager_id)
-    REFERENCES employee(id)
 );
